f548e04f83616a3c8a9eeaa47b970f32090463e1
fydx <lbgg918@gmail.com>
2017-04-05 17:01:15 +0800

    [fix]Fix #4 add import headers to TangramEasyElement Protocol

--- Tangram/Source/Protocols/TangramEasyElementProtocol.h
+++ Tangram/Source/Protocols/TangramEasyElementProtocol.h
@@ -8,6 +8,9 @@
 
 #import <Foundation/Foundation.h>
 #import "TangramDefaultItemModel.h"
+#import "TangramLayoutProtocol.h"
+@class TangramBus;
+
 @protocol TangramEasyElementProtocol <NSObject>
 
 @required
